cb872116c701058cd1381b517a74aebc4433fb39
Lynch00101 <jiricquibrantos870@gmail.com>
2024-09-14 22:51:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "enebe pereng tenge🥺";
+  question.innerHTML = "enebe pereng tenge👉🏻👈🏻🥺";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
